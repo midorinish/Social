85e01526e0de11953e308fe5bede51cedc1aa66d
Silent Dev <rez_a@outlook.com>
2019-07-11 15:22:44 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import fire from "firebase";
+import fire from "firebase/firebase-app";
 
 class App extends Component {
   authListender() {
     fire.auth().onAuthStateChanged(user => {
-      console.log(user);
       if (user) {
         this.setState({ user });
         localStorage.setItem("user", user.uid);
