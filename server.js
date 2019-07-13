b8d8efdc22f34dc3c0e547a37ec7bf40e904721c
Silent Dev <rez_a@outlook.com>
2019-07-12 20:30:50 -0400

    Intial commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
