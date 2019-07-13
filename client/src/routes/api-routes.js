d0078b26799d4612e460d7284f2253900e684448
Silent Dev <rez_a@outlook.com>
2019-07-13 10:37:49 -0400

    Re-created the api-routes and events

--- client/src/routes/api-routes.js
+++ client/src/routes/api-routes.js
@@ -1,18 +1,58 @@
 require("dotenv").config();
 const axios = require("axios");
+const db = require
 const path = require("path");
 
-app.post("/search", (req, res) => {
-  // set eventTitle to the req.body.title with spaces replaced with plus signs(+)
-  let eventTitle = req.body.title.replace(/\s/g, "+");
-  axios
-    .get(
-      `https://api.stubhub.com/sellers/oauth/accesstoken?q=${eventTitle}name=&eventLocalDate=&venue=&city=&state=&country=&apikey=7FbUF2V6b5URGnMS8cySMMGCepFal5GC`
+module.exports = function (app) {
+  app.get("/api/events", (req, res) => {
+    db.Event.find().then(
+      (eventsData) => {
+        res.json(eventsData)
+      }
+    ).catch(
+      (err) => {
+        res.json({ error: err });
+      }
     )
-    .then(response => {
-      res.json(response.data.items);
-    })
-    .catch(err => {
-      res.json({ error: error });
-    });
-});
+  });
+
+  app.post("/search", (req, res) => {
+
+    let eventTitle = req.body.title;
+    axios.get(
+      `https://api.stubhub.com/sellers/oauth/accesstoken?name=&eventLocalDate=&venue=&city=&state=&country?q=${eventTitle}&apikey=${process.env.EventsKey}`
+    ).then(
+      (respons) => {
+        res.json(response.data.items)
+      }
+    ).catch(
+      (err) => {
+        res.json({ error: error })
+      }
+    )
+  });
+
+  app.post("/api/events", (req, res) => {
+    db.Event.create(req.body).then(
+      (response) => {
+        res.json({ successful: response });
+      }
+    ).catch(
+      (err) => {
+        res.json({ error: err });
+      }
+    )
+  });
+
+  app.delete("/api/events/:id", (req, res) => {
+    db.Event.findByIdAndDelete(req.params.id).then(
+      (response) => {
+        res.json({ successful: response });
+      }
+    ).catch(
+      (err) => {
+        res.json({ error: err });
+      }
+    )
+  });
+}
