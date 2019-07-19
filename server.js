b4e0e10ad791ff6b7892d90fcc06af0491a5b377
Mihrimah K <mihrimahk@gmail.com>
2019-07-18 20:47:33 -0400

    add mongoose

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
