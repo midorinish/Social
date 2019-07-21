30c31e06cea89c95a06f8e450832cbff90f7d288
Silent Dev <rez_a@outlook.com>
2019-07-21 14:59:23 -0400

    Removed searchEvents duplicate in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,6 +4,5 @@ export default {
   getEvents: () => axios.get("/api/events"),
   searchEvents: (events) => axios.post("/search", { events }),
   addEventToDb: eventData => axios.post("/api/events", eventData),
-  searchEvents: (events) => axios.post("/search", { events }),
   deleteEvent: id => axios.delete(`/api/events/${id}`),
 }
