867a90d5195575ca31c68d79be4b71252aca46d0
Silent Dev <rez_a@outlook.com>
2019-07-25 07:56:41 -0400

    Merged with 'test' branch and fixed components Search & SearchPage to display search results

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,12 +2,7 @@ import axios from "axios";
 
 export default {
   getEvents: () => axios.get("/api/events"),
-  searchEvents: (events) => axios.post("/searchpage", { events }),
+  searchEvents: (events) => axios.post("/search", { events }),
   addEventToDb: eventData => axios.post("/api/events", eventData),
-<<<<<<< HEAD
-  deleteEvent: id => axios.delete(`/api/events/${id}`)
-
-=======
   deleteEvent: id => axios.delete(`/api/events/${id}`),
->>>>>>> test
 }
